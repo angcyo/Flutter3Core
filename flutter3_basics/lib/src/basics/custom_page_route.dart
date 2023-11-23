835dc1fef6363229f0a331fe3f86709449150223
angcyo <angcyo@126.com>
2023-11-23 23:15:28 +0800

    * fix route transition

--- flutter3_basics/lib/src/basics/custom_page_route.dart
+++ flutter3_basics/lib/src/basics/custom_page_route.dart
@@ -5,13 +5,96 @@ part of flutter3_basics;
 /// @since 2023/11/15
 ///
 
+/// 一样的路由弹窗时的动画处理, 不一样的路由弹窗使用系统默认动画
+mixin SameRouteTransitionMixin<T> on ModalRoute<T> {
+  /// 是否pop相同的路由
+  bool _isPopSameRoute = true;
+
+  @override
+  void didAdd() {
+    debugger();
+    super.didAdd();
+  }
+
+  /// 当前路由即将push
+  @override
+  TickerFuture didPush() {
+    //debugger();
+    return super.didPush();
+  }
+
+  /// 当前路由即将发生变化
+  /// [nextRoute] 下一个路由, 如果有; 如果没有, 则有可能是自身.
+  @override
+  void didChangeNext(Route? nextRoute) {
+    //debugger();
+    _isPopSameRoute = nextRoute == null || nextRoute.runtimeType == runtimeType;
+    super.didChangeNext(nextRoute);
+  }
+
+  /// 完成当前路由显示之后
+  /// [previousRoute] 前一个路由
+  @override
+  void didChangePrevious(Route? previousRoute) {
+    //debugger();
+    super.didChangePrevious(previousRoute);
+  }
+
+  /// 即将完成pop
+  @override
+  void didComplete(T? result) {
+    //debugger();
+    super.didComplete(result);
+  }
+
+  /// 当前路由被移除之后
+  /// [result] 移除时的返回值
+  @override
+  bool didPop(T? result) {
+    //debugger();
+    _isPopSameRoute = true;
+    return super.didPop(result);
+  }
+
+  /// 当前路由上的[nextRoute]路由即将pop
+  @override
+  void didPopNext(Route nextRoute) {
+    //debugger();
+    _isPopSameRoute = nextRoute.runtimeType == runtimeType;
+    super.didPopNext(nextRoute);
+  }
+
+  @override
+  void didReplace(Route? oldRoute) {
+    debugger();
+    super.didReplace(oldRoute);
+  }
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (_isPopSameRoute) {
+      return buildSameTransitions(
+          context, animation, secondaryAnimation, child);
+    }
+    return super
+        .buildTransitions(context, animation, secondaryAnimation, child);
+  }
+
+  /// 需要实现的方法
+  @protected
+  Widget buildSameTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child);
+}
+
 /// https://api.flutter.dev/flutter/animation/Curves-class.html
 /// 底部页面的动画如何控制?
 /// 渐变路由动画
 /// [PageRouteBuilder]
 /// [MaterialPageRoute]
 /// [PageTransitionsTheme]
-class FadePageRoute<T> extends MaterialPageRoute<T> {
+class FadePageRoute<T> extends MaterialPageRoute<T>
+    with SameRouteTransitionMixin<T> {
   FadePageRoute({
     required super.builder,
     super.settings,
@@ -21,7 +104,7 @@ class FadePageRoute<T> extends MaterialPageRoute<T> {
   });
 
   @override
-  Widget buildTransitions(
+  Widget buildSameTransitions(
     BuildContext context,
     Animation<double> animation,
     Animation<double> secondaryAnimation,
@@ -49,7 +132,8 @@ class FadePageRoute<T> extends MaterialPageRoute<T> {
 }
 
 /// 上下滑动路由动画
-class TranslationPageRoute<T> extends MaterialPageRoute<T> {
+class TranslationPageRoute<T> extends MaterialPageRoute<T>
+    with SameRouteTransitionMixin<T> {
   /// 通否同时激活透明渐隐动画
   final bool fade;
 
@@ -63,13 +147,15 @@ class TranslationPageRoute<T> extends MaterialPageRoute<T> {
   });
 
   @override
-  Widget buildTransitions(
+  Widget buildSameTransitions(
     BuildContext context,
     Animation<double> animation,
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
+    //debugger();
     logAnimation("Translation", animation, secondaryAnimation);
+
     //顶部进入动画
     var enter = Tween<Offset>(
       begin: const Offset(0, 1),
@@ -96,7 +182,8 @@ class TranslationPageRoute<T> extends MaterialPageRoute<T> {
 }
 
 /// 左右滑动路由动画
-class SlidePageRoute<T> extends MaterialPageRoute<T> {
+class SlidePageRoute<T> extends MaterialPageRoute<T>
+    with SameRouteTransitionMixin<T> {
   SlidePageRoute({
     required super.builder,
     super.settings,
@@ -121,7 +208,7 @@ class SlidePageRoute<T> extends MaterialPageRoute<T> {
   }
 
   @override
-  Widget buildTransitions(
+  Widget buildSameTransitions(
     BuildContext context,
     Animation<double> animation,
     Animation<double> secondaryAnimation,
