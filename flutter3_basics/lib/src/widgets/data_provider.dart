b2d3a5f87c65b9277cf88f6223db1a7fe597d848
angcyo <angcyo@126.com>
2023-11-24 15:32:57 +0800

    + data provider / value listener

---
+++ flutter3_basics/lib/src/widgets/data_provider.dart
@@ -0,0 +1,32 @@
+part of flutter3_basics;
+
+/// 用来提供数据的一个组件
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @since 2023/11/24
+///
+
+class DataProviderScope extends InheritedWidget {
+  final Object? data;
+
+  const DataProviderScope({
+    super.key,
+    required super.child,
+    this.data,
+  });
+
+  /// 获取一个上层提供的数据
+  /// [DataProviderScope.data]
+  static Object? get(BuildContext context, {bool depend = false}) {
+    if (depend) {
+      return context
+          .dependOnInheritedWidgetOfExactType<DataProviderScope>()
+          ?.data;
+    } else {
+      return context.findAncestorWidgetOfExactType<DataProviderScope>()?.data;
+    }
+  }
+
+  @override
+  bool updateShouldNotify(DataProviderScope oldWidget) =>
+      isDebug || data != oldWidget.data;
+}
---
+++ flutter3_basics/lib/src/widgets/value_listener.dart
@@ -0,0 +1,93 @@
+part of flutter3_basics;
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @since 2023/11/24
+///
+
+typedef ValueWidgetBuilder<T> = Widget Function(BuildContext context, T data);
+
+/// 监听[Listenable]并自动重建小部件
+class ValueListener<T extends Listenable> extends StatefulWidget {
+  final List<T>? listenableList;
+  final ValueWidgetBuilder builder;
+
+  const ValueListener({
+    super.key,
+    required this.builder,
+    this.listenableList,
+  });
+
+  @override
+  State<ValueListener> createState() => _ValueListenerState();
+}
+
+class _ValueListenerState extends State<ValueListener> {
+  void _onValueChanged() {
+    setState(() {});
+  }
+
+  @override
+  void didUpdateWidget(covariant ValueListener<Listenable> oldWidget) {
+    if (widget.listenableList != oldWidget.listenableList) {
+      oldWidget.listenableList?.forEach((element) {
+        element.removeListener(_onValueChanged);
+      });
+      widget.listenableList?.forEach((element) {
+        element.addListener(_onValueChanged);
+      });
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void initState() {
+    widget.listenableList?.forEach((element) {
+      element.addListener(_onValueChanged);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.listenableList?.forEach((element) {
+      element.removeListener(_onValueChanged);
+    });
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context, widget.listenableList);
+  }
+}
+
+extension ValueListenerEx on Listenable {
+  /// 监听[Listenable]并自动重建小部件
+  Widget listener<T extends Listenable>(WidgetBuilder builder) => ValueListener(
+        listenableList: [this],
+        builder: (context, value) => builder(context),
+      );
+
+  /// 监听[Listenable]并自动重建小部件, 并把[value]回调出来
+  Widget listenerValue<T extends Listenable>(ValueWidgetBuilder<T> builder) =>
+      ValueListener(
+        listenableList: [this],
+        builder: (context, value) => builder(context, value as T),
+      );
+}
+
+extension ValueListListenerEx on List<Listenable> {
+  /// 监听[Listenable]并自动重建小部件
+  Widget listener<T extends Listenable>(WidgetBuilder builder) => ValueListener(
+        listenableList: this,
+        builder: (context, value) => builder(context),
+      );
+
+  /// 监听[Listenable]并自动重建小部件, 并把[value]回调出来
+  Widget listenerValue<T extends Listenable>(ValueWidgetBuilder<T> builder) =>
+      ValueListener(
+        listenableList: this,
+        builder: (context, value) => builder(context, value as T),
+      );
+}
