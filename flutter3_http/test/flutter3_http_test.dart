d5ab0275d564779bc8945fa21e437842e37793e5
angcyo <angcyo@126.com>
2024-02-29 22:29:43 +0800

    + http test

--- flutter3_http/test/flutter3_http_test.dart
+++ flutter3_http/test/flutter3_http_test.dart
@@ -1,12 +1,46 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
+import 'dart:io';
 
-import 'package:flutter3_http/flutter3_http.dart';
-
-void main() {
-  test('adds one to input values', () {
-    /*final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);*/
-  });
+void main() async {
+  //await testHttp();
+  await testWebSocket();
+  print('test...end');
 }
+
+/// 使用dart进行简单的http请求测试
+/// https://juejin.cn/post/7173887462219989005
+Future<void> testHttp() async {
+  //InternetAddress.loopbackIPv4;
+
+  const url = "http://www.baidu.com";
+  final client = HttpClient();
+  final request = await client.getUrl(Uri.parse(url));
+  //request.headers.add(name, value);
+  final response = await request.close();
+  final body = await response.transform(const Utf8Decoder()).join();
+  print(body);
+  client.close();
+
+  /*client.getUrl(Uri.parse(url)).then((request) {
+    return request.close();
+  }).then((response) {
+    response.transform(const Utf8Decoder()).listen((data) {
+      print(data);
+    });
+  });*/
+}
+
+/// 使用dart进行简单的websocket测试
+Future<void> testWebSocket() async {
+  const url = "ws://echo.websocket.org";
+  WebSocket socket = await WebSocket.connect(url);
+  socket.add('Hello, World!');
+  await for (var data in socket) {
+    print("from Server: $data");
+    // 关闭连接
+    socket.close();
+  }
+}
+
+/*void test(String | List<int>  data){
+}*/
