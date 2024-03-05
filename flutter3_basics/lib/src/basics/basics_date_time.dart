df989b6afb2f3f7371aa2684b487ece9efbb44cb
angcyo <angcyo@126.com>
2024-03-05 08:57:31 +0800

    * fix base time

--- flutter3_basics/lib/src/basics/basics_date_time.dart
+++ flutter3_basics/lib/src/basics/basics_date_time.dart
@@ -37,25 +37,34 @@ extension TimeEx on int {
   String toTimeString([String? newPattern = "yyyy-MM-dd HH:mm:ss"]) =>
       format(newPattern);
 
-  /// 13位时间戳转换成时间字符串
+  /// 13位时间戳转换成时间字符串, 短时间格式
   /// 转换成刚刚, 几秒前, 几分钟前, 几小时前, 昨天, 前天, 几天前, 几月前, 几年前, 具体时间.
-  String toTimeAgo([String? newPattern = "yyyy-MM-dd HH:mm:ss"]) {
+  /// moment
+  String toTimeAgo(
+      {String just = "刚刚",
+      String s = "秒前",
+      String m = "分钟前",
+      String h = "小时前",
+      String d = "天前",
+      String yesterday = "昨天",
+      String beforeYesterday = "前天",
+      String? timePattern = "yyyy-MM-dd HH:mm:ss"}) {
     //10秒之内, 显示刚刚
     final now = nowTime();
     final diff = now - this;
-    if (diff < 10 * kSecond) return "刚刚";
+    if (diff < 10 * kSecond) return just;
     //1分钟之内, 显示几秒前
-    if (diff < 1 * kMinute) return "$diff秒前";
+    if (diff < 1 * kMinute) return "$diff$s";
     //1小时之内, 显示几分钟前
-    if (diff < 1 * kHour) return "${diff ~/ kMinute}分钟前";
+    if (diff < 1 * kHour) return "${diff ~/ kMinute}$m";
     //1天之内, 显示几小时前
-    if (diff < 1 * kDay) return "${diff ~/ kHour}小时前";
+    if (diff < 1 * kDay) return "${diff ~/ kHour}$h";
     //48小时之内, 显示昨天
-    if (diff < 2 * kDay) return "昨天";
+    if (diff < 2 * kDay) return yesterday;
     //72小时之内, 显示前天
-    if (diff < 3 * kDay) return "前天";
+    if (diff < 3 * kDay) return beforeYesterday;
     //7天之内, 显示几天前
-    if (diff < 7 * kDay) return "${diff ~/ kDay}天前";
-    return format(newPattern);
+    if (diff < 7 * kDay) return "${diff ~/ kDay}$d";
+    return format(timePattern);
   }
 }
