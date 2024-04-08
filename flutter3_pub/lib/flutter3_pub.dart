455e28a516013dcb0265cdde2656dbcc193966c4
angcyo <angcyo@126.com>
2024-04-08 11:02:41 +0800

    * fix base

--- flutter3_pub/lib/flutter3_pub.dart
+++ flutter3_pub/lib/flutter3_pub.dart
@@ -8,19 +8,18 @@ import 'package:flutter/material.dart' as material;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter3_core/flutter3_core.dart';
-import 'package:flutter3_vector/flutter3_vector.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:markdown/markdown.dart';
 
 import 'flutter3_pub.dart';
 
 export 'package:cached_network_image/cached_network_image.dart';
-
 //export 'package:flutter_html/flutter_html.dart';
 //export 'package:flutter_html/html_parser.dart';
 //export 'package:html/parser.dart';
 export 'package:collection/collection.dart';
 export 'package:file_picker/file_picker.dart';
+export 'package:flutter3_vector/flutter3_vector.dart';
 export 'package:flutter_cache_manager/flutter_cache_manager.dart';
 export 'package:flutter_html/flutter_html.dart';
 export 'package:nine_grid_view/nine_grid_view.dart';
@@ -28,20 +27,12 @@ export 'package:octo_image/octo_image.dart';
 export 'package:photo_view/photo_view.dart';
 export 'package:photo_view/photo_view_gallery.dart';
 export 'package:url_launcher/url_launcher.dart';
-export 'package:flutter3_vector/flutter3_vector.dart';
 
 part 'src/archive/zip_ex.dart';
-
 part 'src/http/html_pub.dart';
-
 part 'src/http/markdown_pub.dart';
-
 part 'src/image_pub.dart';
-
 part 'src/launcher_pub.dart';
-
 part 'src/photo/nine_grid_ex.dart';
-
 part 'src/photo/photo_ex.dart';
-
 part 'src/picker/file_picker_ex.dart';
