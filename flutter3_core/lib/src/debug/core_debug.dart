4571ba21ff3bd329d21c5d2d0a811901eb18968f
angcyo <angcyo@126.com>
2024-04-11 09:13:49 +0800

    + debug hive put

--- flutter3_core/lib/src/debug/core_debug.dart
+++ flutter3_core/lib/src/debug/core_debug.dart
@@ -19,17 +19,15 @@ class DebugCommand {
   final String type;
   final String value;
 
-  DebugCommand(
-    this.command,
-    this.key,
-    this.type,
-    this.value,
-  );
+  DebugCommand(this.command,
+      this.key,
+      this.type,
+      this.value,);
 }
 
 class CoreDebug {
   static List<DebugCommandAction> debugCommandActionList =
-      <DebugCommandAction>[];
+  <DebugCommandAction>[];
 
   /// [HiveEx]
   /// [HiveStringEx]
@@ -38,47 +36,103 @@ class CoreDebug {
       return;
     }
     bool match = false;
-    for (var line in lines) {
-      if (line == null) {
-        continue;
-      }
-
-      final keyIndex = line.indexOf("@");
-      final typeIndex = line.indexOf("#");
-      final valueIndex = line.indexOf("=");
-
-      if (keyIndex != -1 && typeIndex != -1 && valueIndex != -1) {
-        //@key#int=value
-        String key = line.substring(keyIndex + 1, typeIndex);
-        final type = line.substring(typeIndex + 1, valueIndex);
-        final valueString = line.substring(valueIndex + 1, line.length);
-
-        bool intercept = false;
-        try {
-          for (var action in debugCommandActionList) {
-            intercept = intercept ||
-                action.call(DebugCommand(line, key, type, valueString));
-          }
-          if (intercept) {
-            match = true;
-          }
-        } catch (e) {
-          assert(() {
-            l.e(e);
-            return true;
-          }());
+    try {
+      for (var line in lines) {
+        if (line == null) {
+          continue;
         }
 
-        //---
+        final keyIndex = line.indexOf("@");
+        final typeIndex = line.indexOf("#");
+        final valueIndex = line.indexOf("=");
 
-        if (!intercept && key.isNotEmpty) {
-          key = key.toLowerCase();
-          switch (key) {
-            case "cmd":
-              break;
+        if (keyIndex != -1 && typeIndex != -1 && valueIndex != -1) {
+          //@key#int=value
+          String key = line.substring(keyIndex + 1, typeIndex);
+          final type = line.substring(typeIndex + 1, valueIndex);
+          final valueString = line.substring(valueIndex + 1, line.length);
+
+          bool intercept = false;
+          try {
+            for (var action in debugCommandActionList) {
+              intercept = intercept ||
+                  action.call(DebugCommand(line, key, type, valueString));
+            }
+            if (intercept) {
+              match = true;
+            }
+          } catch (e) {
+            assert(() {
+              l.e(e);
+              return true;
+            }());
+          }
+
+          //---
+
+          if (!intercept && key.isNotEmpty) {
+            key = key.toLowerCase();
+            switch (key) {
+              case "cmd":
+                break;
+              default:
+              //@key#int=value
+                switch (type) {
+                  case "b":
+                  case "bool":
+                  case "boolean":
+                    final value = valueString.toBoolOrNull();
+                    if (value == null) {
+                      key.hiveDelete();
+                    } else {
+                      key.hivePut(value);
+                    }
+                    match = true;
+                    break;
+                  case "int":
+                  case "i":
+                  case "long":
+                  case "l":
+                    final value = valueString.toIntOrNull();
+                    if (value == null) {
+                      key.hiveDelete();
+                    } else {
+                      key.hivePut(value);
+                    }
+                    match = true;
+                    break;
+                  case "float":
+                  case "f":
+                  case "double":
+                  case "d":
+                    final value = valueString.toDoubleOrNull();
+                    if (value == null) {
+                      key.hiveDelete();
+                    } else {
+                      key.hivePut(value);
+                    }
+                    match = true;
+                    break;
+                  case "string":
+                  case "s":
+                    key.hivePut(valueString);
+                    match = true;
+                    break;
+                }
+                break;
+            }
           }
         }
       }
+    } catch (e) {
+      assert(() {
+        l.e(e);
+        return true;
+      }());
+    }
+
+    if (match) {
+      editText?._feedback()
     }
   }
 }
