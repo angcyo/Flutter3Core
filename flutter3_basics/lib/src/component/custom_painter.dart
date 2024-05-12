6ce0b9fba2fb03e05d6213988eb6c5165cd5c5d0
angcyo <angcyo@126.com>
2024-05-12 12:31:40 +0800

    + TrianglePainter

--- flutter3_basics/lib/src/component/custom_painter.dart
+++ flutter3_basics/lib/src/component/custom_painter.dart
@@ -39,3 +39,35 @@ CustomPaint paintWidget(
       isComplex: isComplex,
       willChange: willChange,
     );
+
+///  绘制一个三角形
+class TrianglePainter extends CustomPainter {
+  /// 三角形的颜色
+  final Color color;
+
+  const TrianglePainter({required this.color});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    final path = Path();
+    paint.isAntiAlias = true;
+    paint.color = color;
+
+    path.lineTo(size.width * 0.66, size.height * 0.86);
+    path.cubicTo(size.width * 0.58, size.height * 1.05, size.width * 0.42,
+        size.height * 1.05, size.width * 0.34, size.height * 0.86);
+    path.cubicTo(size.width * 0.34, size.height * 0.86, 0, 0, 0, 0);
+    path.cubicTo(0, 0, size.width, 0, size.width, 0);
+    path.cubicTo(size.width, 0, size.width * 0.66, size.height * 0.86,
+        size.width * 0.66, size.height * 0.86);
+    path.cubicTo(size.width * 0.66, size.height * 0.86, size.width * 0.66,
+        size.height * 0.86, size.width * 0.66, size.height * 0.86);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
