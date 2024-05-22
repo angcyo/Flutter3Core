8db00b464c1cfd1c9ab0a6823903e7e8f26d4de0
angcyo <angcyo@126.com>
2024-05-22 18:37:00 +0800

    + svg test

---
+++ flutter3_vector/test/svg_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @date 2024/05/22
+///
+
+void main() {
+  test('test svg 1', () {
+    const width = 160;
+    const height = width;
+    //Path path = Path()..addRRect(rrect)
+  });
+}
