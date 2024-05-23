991e2c4bad5527db84ce7f311cca2e7d31cfce91
angcyo <angcyo@126.com>
2024-05-23 10:31:54 +0800

    + path test

--- flutter3_vector/test/svg_test.dart
+++ flutter3_vector/test/svg_test.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter3_basics/flutter3_basics.dart';
+import 'package:flutter3_vector/flutter3_vector.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 ///
@@ -6,9 +9,57 @@ import 'package:flutter_test/flutter_test.dart';
 ///
 
 void main() {
-  test('test svg 1', () {
-    const width = 160;
-    const height = width;
-    //Path path = Path()..addRRect(rrect)
+
+  test('test svg path', () {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    @mm
+    final width = 160.0 /*.toDpFromMm()*/;
+    final height = 160.0 /*.toDpFromMm()*/;
+
+    @mm
+    final bestWidth = 160.0;
+    final bestHeight = 120.0;
+
+    final validWidth = bestWidth * 0.6;
+    final validHeight = bestHeight * 0.6;
+
+    final l = (width - bestWidth) / 2;
+    final t = (height - bestHeight) / 2;
+    final r = l + bestWidth;
+    final b = t + bestHeight;
+    final cx = (l + r) / 2;
+    final cy = (t + b) / 2;
+
+    //debugger();
+    final buffer = StringBuffer();
+
+    final Path path = Path();
+    path.moveTo(l, cy - validHeight / 2);
+    path.quadraticBezierTo(l, t, cx - validWidth / 2, t);
+    buffer.write('M $l ${cy - validHeight / 2} ');
+    buffer.write('Q $l $t ${cx - validWidth / 2} $t ');
+
+    path.lineTo(cx + validWidth / 2, t);
+    path.quadraticBezierTo(r, t, r, cy - validHeight / 2);
+    buffer.write('L ${cx + validWidth / 2} $t ');
+    buffer.write('Q $r $t $r ${cy - validHeight / 2} ');
+
+    path.lineTo(r, cy + validHeight / 2);
+    path.quadraticBezierTo(r, b, cx + validWidth / 2, b);
+    buffer.write('L $r ${cy + validHeight / 2} ');
+    buffer.write('Q $r $b ${cx + validWidth / 2} $b ');
+
+    path.lineTo(cx - validWidth / 2, b);
+    path.quadraticBezierTo(l, b, l, cy + validHeight / 2);
+    buffer.write('L ${cx - validWidth / 2} $b ');
+    buffer.write('Q $l $b $l ${cy + validHeight / 2} ');
+
+    path.lineTo(l, cy - validHeight / 2);
+    buffer.write('L $l ${cy - validHeight / 2} ');
+
+    final svgPath = path.toSvgPathString();
+    consoleLog(svgPath);
+    consoleLog(buffer);
   });
 }
