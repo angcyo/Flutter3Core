f5e7f98fb568135ed65bf1be8cc8582df58f5efa
angcyo <angcyo@126.com>
2024-07-02 09:05:35 +0800

    * fix base

--- flutter3_widgets/lib/src/widgets/app/text_field.dart
+++ flutter3_widgets/lib/src/widgets/app/text_field.dart
@@ -111,7 +111,7 @@ class TextFieldConfig {
     //debugger();
     if (updateFieldValueFn == null) {
       assert(() {
-        l.w('无效的操作');
+        l.w('无效更新TextEditingValue[$text]');
         return true;
       }());
     }
