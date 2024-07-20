409d82c40183b7a682482cfffc23042a07b33385
angcyo <angcyo@126.com>
2024-07-20 10:28:07 +0800

    * fix ffi

--- flutter3_ffi/lib/src/ffi_ex.dart
+++ flutter3_ffi/lib/src/ffi_ex.dart
@@ -9,12 +9,13 @@ part of '../flutter3_ffi.dart';
 extension FfiListIntEx on List<int> {
   /// 转成[Vec_uint8_t], 并自动释放内存
   R? withVecUint8<R>(R? Function(ffi.Pointer<Vec_uint8_t> ptr) action) {
-    final watch = Stopwatch()..start();
+    final watch = Stopwatch()
+      ..start();
     final bytes = this;
     //创建一个指针, 用来ffi传递
     //分配内存: 55ms
     final ffi.Pointer<ffi.Uint8> bytesPtr =
-        calloc.allocate<ffi.Uint8>(bytes.length);
+    calloc.allocate<ffi.Uint8>(bytes.length);
     final Uint8List nativeBytes = bytesPtr.asTypedList(bytes.length);
     nativeBytes.setAll(0, bytes);
 
@@ -29,7 +30,8 @@ extension FfiListIntEx on List<int> {
       if (kDebugMode) {
         debugPrint('分配内存: ${watch.elapsedMilliseconds}ms');
       }
-      final watch2 = Stopwatch()..start();
+      final watch2 = Stopwatch()
+        ..start();
       //执行耗时: 4688ms
       final result = action(ptr);
       watch2.stop();
@@ -57,6 +59,20 @@ extension FfiVecUint8Ex on Vec_uint8_t {
     return reversedBytes;
   }
 
+  /// rgba像素字节数据转成图片
+  Future<ui.Image> toImageFromPixels(int width, int height,
+      [ui.PixelFormat format = ui.PixelFormat.rgba8888]) {
+    final Completer<ui.Image> completer = Completer<ui.Image>();
+    ui.decodeImageFromPixels(
+      toBytes(),
+      width,
+      height,
+      format,
+      completer.complete,
+    );
+    return completer.future;
+  }
+
   /// 转成字符串
   String toStr() => utf8.decode(toBytes());
 }
@@ -73,15 +89,6 @@ extension FfiStringEx on String {
 
 extension FfiPixelsImageEx on PixelsImage {
   /// 转成图片
-  Future<ui.Image> toImage([ui.PixelFormat format = ui.PixelFormat.rgba8888]) {
-    final Completer<ui.Image> completer = Completer<ui.Image>();
-    ui.decodeImageFromPixels(
-      pixels.toBytes(),
-      w,
-      h,
-      format,
-      completer.complete,
-    );
-    return completer.future;
-  }
+  Future<ui.Image> toImage([ui.PixelFormat format = ui.PixelFormat.rgba8888]) =>
+      pixels.toImageFromPixels(w, h, format);
 }
