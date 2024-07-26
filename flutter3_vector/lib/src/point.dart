6c7e387005513d9123cf351213e29d8503faa23b
angcyo <angcyo@126.com>
2024-07-26 10:32:35 +0800

    * fix base ex

--- flutter3_vector/lib/src/point.dart
+++ flutter3_vector/lib/src/point.dart
@@ -35,3 +35,78 @@ final class Point {
     return 'Point{x:$x, y:$y a:$a}';
   }
 }
+
+extension ListPointEx on List<Point> {
+  /// 转换成一维数组点信息
+  /// [x,y,x,y,x,y,x,y...]
+  List<double> toPointList() {
+    List<double> result = [];
+    for (final point in this) {
+      result.add(point.x);
+      result.add(point.y);
+    }
+    return result;
+  }
+
+  /// x,y,x,y...
+  String toPointText() {
+    StringBuffer buffer = StringBuffer();
+    for (final point in this) {
+      buffer.write('${point.x},${point.y}');
+    }
+    return buffer.toString();
+  }
+}
+
+extension ListListPointEx on List<List<Point>> {
+  /// 转换成二维数组点信息
+  List<List<double>> toPointListList() {
+    List<List<double>> result = [];
+    for (final pointList in this) {
+      result.add(pointList.toPointList());
+    }
+    return result;
+  }
+
+  /// 用换行分割多段
+  String toPointText() {
+    StringBuffer buffer = StringBuffer();
+    for (final pointList in this) {
+      buffer.writeln(pointList.toPointText());
+    }
+    return buffer.toString();
+  }
+}
+
+extension ListListStringEx on String {
+  /// 反向解析
+  List<Point> toPointList() {
+    List<Point> result = [];
+    List<String> points = split(',');
+    for (int i = 0; i < points.length; i += 2) {
+      try {
+        result.add(
+            Point(double.parse(points[i]), double.parse(points[i + 1]), null));
+      } catch (e, s) {
+        assert(() {
+          printError(e, s);
+          return true;
+        }());
+      }
+    }
+    return result;
+  }
+
+  /// 反向解析
+  List<List<Point>> toPointListList() {
+    List<List<Point>> result = [];
+    List<String> lines = this.lines();
+    for (final line in lines) {
+      List<Point> pointList = line.toPointList();
+      if (pointList.isNotEmpty) {
+        result.add(pointList);
+      }
+    }
+    return result;
+  }
+}
