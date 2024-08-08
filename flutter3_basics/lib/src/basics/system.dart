d51cad98db361ea152f72db0123133b705d724f6
angcyo <angcyo@126.com>
2024-08-08 14:20:26 +0800

    * fix screen orientation

--- flutter3_basics/lib/src/basics/system.dart
+++ flutter3_basics/lib/src/basics/system.dart
@@ -6,16 +6,41 @@ part of '../../flutter3_basics.dart';
 ///
 
 /// 设置屏幕方向
+///
+/// [OrientationBuilder]
+///
+/// ```
+/// MediaQuery.of(context).orientation;
+/// '屏幕方向: ${orientation == Orientation.portrait ? "纵向" : "横向"}',
+/// ```
+///
 /// [DeviceOrientation.landscapeRight] // 横屏模式，向右旋转
 /// [DeviceOrientation.landscapeLeft] // 横屏模式 向左旋转
-void setScreenOrientation(DeviceOrientation orientation) {
-  SystemChrome.setPreferredOrientations([orientation]);
+void setScreenOrientation([DeviceOrientation? orientation]) {
+  SystemChrome.setPreferredOrientations(
+      orientation == null ? [] : [orientation]);
+}
+
+void setScreenOrientations([List<DeviceOrientation>? orientations]) {
+  SystemChrome.setPreferredOrientations(orientations ?? []);
 }
 
 /// 设置横屏
 void setScreenLandscape(
-    [DeviceOrientation orientation = DeviceOrientation.landscapeLeft]) {
-  setScreenOrientation(orientation);
+    [List<DeviceOrientation> orientations = const [
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight
+    ]]) {
+  setScreenOrientations(orientations);
+}
+
+/// 设置竖屏
+void setScreenPortrait(
+    [List<DeviceOrientation> orientations = const [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]]) {
+  setScreenOrientations(orientations);
 }
 
 //设置状态栏样式
