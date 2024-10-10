c110345fd950b695319f8cd7460f40a0fb6bda1e
angcyo <angcyo@126.com>
2024-10-10 14:42:12 +0800

    * fix base

--- flutter3_ffi/lib/src/ffi_ex.dart
+++ flutter3_ffi/lib/src/ffi_ex.dart
@@ -12,7 +12,7 @@ extension FfiListIntEx on List<int> {
     final bytes = this;
     //创建一个指针, 用来ffi传递
     final ffi.Pointer<ffi.Uint8> bytesPtr =
-    calloc.allocate<ffi.Uint8>(bytes.length);
+        calloc.allocate<ffi.Uint8>(bytes.length);
     final Uint8List nativeBytes = bytesPtr.asTypedList(bytes.length);
     nativeBytes.setAll(0, bytes);
 
@@ -28,14 +28,13 @@ extension FfiListIntEx on List<int> {
   R? withVecUint8<R>(R? Function(ffi.Pointer<Vec_uint8_t> ptr) action) {
     Stopwatch? watch;
     if (kDebugMode) {
-      watch = Stopwatch()
-        ..start();
+      watch = Stopwatch()..start();
     }
     final bytes = this;
     //创建一个指针, 用来ffi传递
     //分配内存: 55ms
     final ffi.Pointer<ffi.Uint8> bytesPtr =
-    calloc.allocate<ffi.Uint8>(bytes.length);
+        calloc.allocate<ffi.Uint8>(bytes.length);
     final Uint8List nativeBytes = bytesPtr.asTypedList(bytes.length);
     nativeBytes.setAll(0, bytes);
 
@@ -52,8 +51,7 @@ extension FfiListIntEx on List<int> {
       }
       Stopwatch? watch2;
       if (kDebugMode) {
-        watch2 = Stopwatch()
-          ..start();
+        watch2 = Stopwatch()..start();
       }
       //执行耗时: 4688ms
       final result = action(ptr);
@@ -109,7 +107,7 @@ extension FfiListDoubleEx on List<double> {
     final bytes = this;
     //创建一个指针, 用来ffi传递
     final ffi.Pointer<ffi.Double> bytesPtr =
-    calloc.allocate<ffi.Double>(bytes.length);
+        calloc.allocate<ffi.Double>(bytes.length);
     final Float64List nativeBytes = bytesPtr.asTypedList(bytes.length);
     nativeBytes.setAll(0, bytes);
     //ffi传递的结构体
@@ -154,14 +152,12 @@ extension FfiListListDoubleEx on List<List<double>> {
     final bytes = this;
     //创建一个指针, 用来ffi传递
     final ffi.Pointer<Vec_double_t> bytesPtr =
-    calloc.allocate<Vec_double_t>(bytes.length);
+        calloc.allocate<Vec_double_t>(bytes.length);
 
     for (var i = 0; i < bytes.length; i++) {
       final list = bytes[i];
       //list.toVecDouble()
-      final ref = list
-          .toVecDouble()
-          .ref;
+      final ref = list.toVecDouble().ref;
       bytesPtr[i] = ref;
       //bytesPtr.elementAt(i);
       //bytesPtr += ref;
@@ -262,8 +258,10 @@ extension FfiPixelsImageEx on PixelsImage {
 }
 
 /// 批量创建[Vec_uint8_t]指针
-R? ffiPtrList<R>(R? Function(List<ffi.Pointer<Vec_uint8_t>> ptrList) action,
-    List<dynamic> args,) {
+R? ffiPtrList<R>(
+  R? Function(List<ffi.Pointer<Vec_uint8_t>> ptrList) action,
+  List<dynamic> args,
+) {
   final ptrList = <ffi.Pointer<Vec_uint8_t>>[];
   for (var i = 0; i < args.length; i++) {
     final arg = args[i];
@@ -273,6 +271,11 @@ R? ffiPtrList<R>(R? Function(List<ffi.Pointer<Vec_uint8_t>> ptrList) action,
       ptrList.add(arg.toVecUint8());
     } else if (arg is ffi.Pointer<Vec_uint8_t>) {
       ptrList.add(arg);
+    } else {
+      assert(() {
+        print("无法处理的类型: [${arg.runtimeType}]");
+        return true;
+      }());
     }
   }
   try {
