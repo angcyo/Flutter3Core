541bd74697be7d533ccbe1a856521d96fdeb360a
angcyo <angcyo@126.com>
2024-11-02 16:15:43 +0800

    + android move to back

---
+++ flutter3_app/lib/src/android_app.dart
@@ -0,0 +1,13 @@
+part of '../flutter3_app.dart';
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @date 2024/11/02
+///
+
+/// 仅支持Android平台
+Future moveTaskToBack([bool nonRoot = true]) async {
+  if (isAndroid) {
+    FlutterMoveTaskBack.moveTaskToBack(nonRoot: nonRoot);
+  }
+}
