0e80a65ccbcb5784fc4400b1f585a13527dbb37b
angcyo <angcyo@126.com>
2024-12-21 11:11:21 +0800

    * fix meta

--- flutter3_basics/lib/src/meta/meta.dart
+++ flutter3_basics/lib/src/meta/meta.dart
@@ -110,6 +110,17 @@ class Dp {
 
 const dp = Dp();
 
+class Initialize {
+  final String des;
+
+  const Initialize([this.des = '必要的初始化操作']);
+
+  @override
+  String toString() {
+    return 'Initialize{des: $des}';
+  }
+}
+
 //region---单位
 
 const unit = AnnotationMeta('当前的值根据设置的Unit自动适配');
@@ -141,7 +152,7 @@ const property = AnnotationMeta('可以被操作的属性');
 const configProperty = AnnotationMeta('当前属性用来配置');
 const flagProperty = AnnotationMeta('标记属性, 不参与底层的逻辑运算');
 const indirectProperty = AnnotationMeta('标记间接属性, 间接属性不参与直接计算,而是创建确实的直接属性');
-const initialize = AnnotationMeta('必要的初始化操作');
+const initialize = Initialize();
 const streamMark = AnnotationMeta('当前操作会触发流的通知, 包含流的操作');
 const updateSignalMark = AnnotationMeta('一个更新信号通知,监听此通知实现界面更新');
 const autoInjectMark = AnnotationMeta('标识当前方法/属性会在框架内自动注入');
