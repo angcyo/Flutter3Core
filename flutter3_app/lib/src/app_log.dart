ac6fa12ec49c8f2992a2acbe25a03c44dba911a9
angcyo <angcyo@126.com>
2025-04-24 20:31:05 +0800

    + share log

--- flutter3_app/lib/src/app_log.dart
+++ flutter3_app/lib/src/app_log.dart
@@ -1,5 +1,44 @@
 part of '../flutter3_app.dart';
 
+extension AppLogPathListEx on List<String> {
+  /// 压缩文件/文件夹
+  /// [output] zip文件输出全路径
+  Future zipPathList(String output) async {
+    await zip(output,
+        action: (encoder) {
+          final hiveJson = hiveAll()?.toJsonString();
+          if (hiveJson != null && hiveJson.isNotEmpty) {
+            encoder.writeStringSync(hiveJson, 'hive.json');
+          }
+          final lastInfo = DebugPage.lastDebugCopyStringBuilder(
+              GlobalConfig.def.globalContext);
+          if (lastInfo != null && lastInfo.isNotEmpty) {
+            encoder.writeStringSync(lastInfo, 'last_debug_info.log');
+          }
+        },
+        onGetFileName: (path) => zipEntryKeyMap[path]);
+    assert(() {
+      final log =
+          "压缩完成[${size()}]:$output :${(output.file().fileSizeSync()).toSizeStr()}";
+      l.i(log);
+      return true;
+    }());
+  }
+}
+
+extension AppLogDirectoryEx on Directory {
+  /// 分享文件夹对应的日志
+  /// [shareAppLog]
+  Future shareAppLog([String? logName]) async {
+    final info = await $platformPackageInfo;
+    final output = await cacheFilePath(logName?.ensureSuffix(".zip") ??
+        "LOG_${info.appName}_${info.version}_${info.buildNumber}_${nowTimeString("yyyy-MM-dd_HH-mm-ss_SSS")}.zip");
+    final list = <String>[path];
+    await list.zipPathList(output);
+    output.shareFile().ignore();
+  }
+}
+
 ///
 /// Email:angcyo@126.com
 /// @author angcyo
@@ -21,26 +60,7 @@ Future shareAppLog([String? logName]) async {
   list.addAll(tempShareLogPathList);
   list.addAll(globalShareLogPathList);
   //--
-  list
-      .zip(output,
-          action: (encoder) {
-            final hiveJson = hiveAll()?.toJsonString();
-            if (hiveJson != null && hiveJson.isNotEmpty) {
-              encoder.writeStringSync(hiveJson, 'hive.json');
-            }
-            final lastInfo = DebugPage.lastDebugCopyStringBuilder(
-                GlobalConfig.def.globalContext);
-            if (lastInfo != null && lastInfo.isNotEmpty) {
-              encoder.writeStringSync(lastInfo, 'last_debug_info.log');
-            }
-          },
-          onGetFileName: (path) => zipEntryKeyMap[path])
-      .ignore();
-  assert(() {
-    final log = "压缩完成:$output :${(output.file().fileSizeSync()).toSizeStr()}";
-    l.i(log);
-    return true;
-  }());
+  await list.zipPathList(output);
   output.shareFile().ignore();
   clearTempShareLogPath();
 }
