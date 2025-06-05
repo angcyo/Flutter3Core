c045a750646c8cb40527ad2afd664c2edd24a059
angcyo <angcyo@126.com>
2025-06-05 11:51:29 +0800

    * fix base

--- flutter3_basics/test/isolate_test.dart
+++ flutter3_basics/test/isolate_test.dart
@@ -10,4 +10,13 @@ void main() async {
 
   // 使用 compute 函数创建新的 Isolate
   //await compute(isolateFunction, 'Hello from main isolate!');
+  await Isolate.spawn(isolateFunction, 'Hello from main isolate!');
+
+  //等待
+  await Future.delayed(Duration(seconds: 1));
+}
+
+void isolateFunction(String message) {
+  print('Isolate Name: ${Isolate.current.debugName}');
+  print(message);
 }
