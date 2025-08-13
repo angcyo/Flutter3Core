9ee426cabba7f964712806001d7f045f5a7b35c2
angcyo <angcyo@126.com>
2025-08-13 16:47:13 +0800

    + test

--- flutter3_basics/test/basics_test.dart
+++ flutter3_basics/test/basics_test.dart
@@ -1,3 +1,5 @@
+
+
 ///
 /// Email:angcyo@126.com
 /// @author angcyo
@@ -37,4 +39,23 @@ void main() {
   for (var i = 0; i < count; i++) {
     print((min + step2 * i).round());
   }
+
+  final maxBase36Value = int.parse("1z141z4", radix: 36); // 4294967296
+  final maxBase36ValueStr = 65535.toRadixString(36); // 1ekf
+  print("...");
+  print(maxBase36Value);
+  print(maxBase36ValueStr);
+
+  //Radix 48 not in range 2..36
+  final maxBase48Value = int.parse("11414", radix: 18); // 112126
+  final maxBase48ValueStr = 65535.toRadixString(18); // b44f
+  print("...");
+  print(maxBase48Value);
+  print(maxBase48ValueStr);
+
+  final maxBase19Value = int.parse("11414", radix: 19); // 138647
+  final maxBase19ValueStr = 65535.toRadixString(19); // 9aa4
+  print("...");
+  print(maxBase19Value);
+  print(maxBase19ValueStr);
 }
