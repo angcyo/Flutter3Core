2fa83e5bdcfe581daa61638328c2851bb565b30d
angcyo <angcyo@126.com>
2025-10-10 11:39:31 +0800

    + debug

--- flutter3_basics/lib/src/widgets/bounds_widget.dart
+++ flutter3_basics/lib/src/widgets/bounds_widget.dart
@@ -15,12 +15,15 @@ class BoundsWidget extends SingleChildRenderObjectWidget {
   /// 圆角大小
   final double radius;
 
+  final String? debugLabel;
+
   const BoundsWidget({
     super.key,
     super.child,
     this.color = Colors.red,
     this.strokeWidth = 1,
     this.radius = 0,
+    this.debugLabel,
   });
 
   @override
@@ -43,6 +46,24 @@ class _BoundsWidgetRenderObject extends RenderProxyBox {
   BoundsWidget config;
 
   _BoundsWidgetRenderObject(this.config);
+
+  @override
+  void attach(PipelineOwner owner) {
+    debugger(when: config.debugLabel != null);
+    super.attach(owner);
+  }
+
+  @override
+  void detach() {
+    debugger(when: config.debugLabel != null);
+    super.detach();
+  }
+
+  @override
+  void dispose() {
+    debugger(when: config.debugLabel != null);
+    super.dispose();
+  }
 
   @override
   void paint(PaintingContext context, ui.Offset offset) {
@@ -68,13 +89,17 @@ class _BoundsWidgetRenderObject extends RenderProxyBox {
 extension BoundsWidgetEx on Widget {
   /// [BoundsWidget]
   Widget bounds({
+    Key? key,
     Color color = Colors.red,
     double strokeWidth = 1,
     double radius = 0,
+    String? debugLabel,
   }) => BoundsWidget(
+    key: key,
     color: color,
     strokeWidth: strokeWidth,
     radius: radius,
+    debugLabel: debugLabel,
     child: this,
   );
 }
