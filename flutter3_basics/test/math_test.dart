f3c75ca733e8f1fbc92375a5d354704ad095059d
angcyo <angcyo@126.com>
2026-03-06 18:09:18 +0800

    + test

---
+++ flutter3_basics/test/math_test.dart
@@ -0,0 +1,12 @@
+import 'dart:math' as math;
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @date 2026/03/06
+///
+/// 数学函数测试
+void main() {
+  print(math.asin(0));
+  print(math.asin(0.1));
+  print(math.asin(1));
+}
