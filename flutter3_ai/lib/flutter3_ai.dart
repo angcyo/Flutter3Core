db3ee4bce2112c0ccba353026d21075e214c5676
angcyo <angcyo@126.com>
2026-03-20 20:16:48 +0800

    - remove gemma for windows

--- flutter3_ai/lib/flutter3_ai.dart
+++ flutter3_ai/lib/flutter3_ai.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter3_ai/src/gemma/chat_message.dart';
 import 'package:flutter3_ai/src/gemma/thinking_widget.dart';
 import 'package:flutter3_basics/flutter3_basics.dart';
-import 'package:flutter_gemma/core/chat.dart';
-import 'package:flutter_gemma/core/message.dart';
-import 'package:flutter_gemma/core/model_response.dart';
+//import 'package:flutter_gemma/core/chat.dart';
+//import 'package:flutter_gemma/core/message.dart';
+//import 'package:flutter_gemma/core/model_response.dart';
 
-export 'package:flutter_gemma/flutter_gemma.dart';
+//export 'package:flutter_gemma/flutter_gemma.dart';
 
-part 'src/gemma/gemma_input_field.dart';
-part 'src/gemma/gemma_service.dart';
+//part 'src/gemma/gemma_input_field.dart';
+//part 'src/gemma/gemma_service.dart';
 
 ///
 /// @author <a href="mailto:angcyo@126.com">angcyo</a>
--- flutter3_ai/lib/src/gemma/gemma_service.dart
+++ flutter3_ai/lib/src/gemma/gemma_service.dart
@@ -1,39 +1,39 @@
-part of '../../flutter3_ai.dart';
-
-///
-/// @author <a href="mailto:angcyo@126.com">angcyo</a>
-/// @date 2026/03/10
-///
-/// https://github.com/DenisovAV/flutter_gemma/blob/main/example/lib/services/gemma_service.dart
-class GemmaLocalService {
-  final InferenceChat _chat;
-
-  GemmaLocalService(this._chat);
-
-  Future<void> addQuery(Message message) => _chat.addQuery(message);
-
-  /// Process message and return stream with sync/async mode support
-  Future<Stream<ModelResponse>> processMessage(
-    Message message, {
-    bool useSyncMode = false,
-  }) async {
-    l.d('GemmaLocalService: processMessage() called with: "${message.text}"');
-    l.d('GemmaLocalService: Adding query to chat: "${message.text}"');
-    await _chat.addQuery(message);
-
-    if (useSyncMode) {
-      l.d('GemmaLocalService: Using SYNC mode');
-      final response = await _chat.generateChatResponse();
-      return Stream.fromIterable([response]);
-    } else {
-      l.d('GemmaLocalService: Using ASYNC streaming mode');
-      return _chat.generateChatResponseAsync();
-    }
-  }
-
-  /// Legacy method for backward compatibility
-  Stream<ModelResponse> processMessageAsync(Message message) async* {
-    await _chat.addQuery(message);
-    yield* _chat.generateChatResponseAsync();
-  }
-}
+// part of '../../flutter3_ai.dart';
+//
+// ///
+// /// @author <a href="mailto:angcyo@126.com">angcyo</a>
+// /// @date 2026/03/10
+// ///
+// /// https://github.com/DenisovAV/flutter_gemma/blob/main/example/lib/services/gemma_service.dart
+// class GemmaLocalService {
+//   final InferenceChat _chat;
+//
+//   GemmaLocalService(this._chat);
+//
+//   Future<void> addQuery(Message message) => _chat.addQuery(message);
+//
+//   /// Process message and return stream with sync/async mode support
+//   Future<Stream<ModelResponse>> processMessage(
+//     Message message, {
+//     bool useSyncMode = false,
+//   }) async {
+//     l.d('GemmaLocalService: processMessage() called with: "${message.text}"');
+//     l.d('GemmaLocalService: Adding query to chat: "${message.text}"');
+//     await _chat.addQuery(message);
+//
+//     if (useSyncMode) {
+//       l.d('GemmaLocalService: Using SYNC mode');
+//       final response = await _chat.generateChatResponse();
+//       return Stream.fromIterable([response]);
+//     } else {
+//       l.d('GemmaLocalService: Using ASYNC streaming mode');
+//       return _chat.generateChatResponseAsync();
+//     }
+//   }
+//
+//   /// Legacy method for backward compatibility
+//   Stream<ModelResponse> processMessageAsync(Message message) async* {
+//     await _chat.addQuery(message);
+//     yield* _chat.generateChatResponseAsync();
+//   }
+// }
